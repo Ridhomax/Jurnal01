ad3bd9b815c52250f8ca00abeff978e2c7cc2339
Ridhomax <abuhurairoh172@gmail.com>
2023-09-27 11:01:05 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "M. Ridho Masriyan";
+        String nim = "607062300061";
+        String kelas = "D3IF-47-04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
